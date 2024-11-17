edb367e73deb33c53bcec06dbbdd7dc9e92d5270
SLHOKE314 <shelby.hoke@my.strayer.edu>
2024-11-17 10:42:12 -0600

    account.java

---
+++ .github/workflows/account.java
@@ -0,0 +1,37 @@
+public class Account {
+    private String username;
+    private String password;
+    private double balance;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+        this.balance = 0.0; // Initial balance is 0
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount; // Only allow positive deposits
+        } else {
+            throw new IllegalArgumentException("Deposit amount must be positive.");
+        }
+    }
+
+    public void withdraw(double amount) throws Exception {
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Withdrawal amount must be positive.");
+        }
+        if (amount > balance) {
+            throw new Exception("Insufficient funds.");
+        }
+        balance -= amount;
+    }
+}
